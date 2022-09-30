544450876b2e3e501370bcd15a5e9819668950f2
Muhammad Abdullah Khan <abdullahkhan.smiu@gmail.com>
2022-10-01 02:07:15 +0500

    'Commit;

---
+++ Day 5/app.js
@@ -0,0 +1,22 @@
+const panels = document.querySelectorAll(
+    '.panel'
+)
+
+function toggle() {
+    this.classList.toggle('open')
+}
+
+function toggleActive(e) {
+    console.log(e.propertyName)
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active')
+
+    }
+}
+panels.forEach(panel => panel.addEventListener('click', toggle))
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive))
+
+
+
+
+ 
